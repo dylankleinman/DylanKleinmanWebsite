5794b0ac1c2fd88b6ef7a13d41e44af2ba6986cf
dylankleinman <dylankleinman@yahoo.com>
2019-03-05 23:27:06 -0800

    Finally got slick to work.  Needed to add the js file to the bottom of the body, not the head.

---
+++ js/Carousel.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  console.log('here');
+  $('.your-class').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+});
+});
