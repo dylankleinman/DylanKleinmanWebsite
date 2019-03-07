dd79ea8d8a8b671c586b1e995b36c1c40382f435
Kleinman <dylan.kleinman@linquest.com>
2019-03-07 13:40:01 -0800

    added correct scrolling images and padding

--- js/Carousel.js
+++ js/Carousel.js
@@ -3,9 +3,11 @@ $(document).ready(function(){
   $('.slider').slick({
     dots: true,
     infinite: true,
-    speed: 300,
+    speed: 600,
     slidesToShow: 1,
     fade: true,
-    cssEase: 'linear'
+    cssEase: 'linear',
+    nextArrow: '<a class="carousel-control-next" role="button"><span class="pointer carousel-control-next-icon" aria-hidden="true"></span></a>',
+    prevArrow: '<a class="carousel-control-prev" role="button"><span class="pointer carousel-control-prev-icon" aria-hidden="true"></span></a>',
   });
 });
