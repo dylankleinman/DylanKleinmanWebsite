7844649462e098bad5d5963bcca7454592b931bb
dylankleinman <dylankleinman@yahoo.com>
2019-03-08 06:50:41 -0800

    added in new images

--- js/Carousel.js
+++ js/Carousel.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
     slidesToShow: 1,
     fade: true,
     cssEase: 'linear',
-    nextArrow: '<a class="carousel-control-next" role="button"><span class="pointer carousel-control-next-icon" aria-hidden="true"></span></a>',
-    prevArrow: '<a class="carousel-control-prev" role="button"><span class="pointer carousel-control-prev-icon" aria-hidden="true"></span></a>',
+    nextArrow: '<a class="pointer carousel-control-next" role="button"><span class=" carousel-control-next-icon" aria-hidden="true"></span></a>',
+    prevArrow: '<a class="pointer carousel-control-prev" role="button"><span class=" carousel-control-prev-icon" aria-hidden="true"></span></a>',
   });
 });
